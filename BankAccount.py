d5b71bac78f5aa510442c42442b3f3f05363ad55
rupang47 <rupang47@naver.com>
2023-05-23 15:51:46 +0900

    클래스 연습 완료

--- BankAccount.py
+++ BankAccount.py
@@ -2,19 +2,28 @@
 
 #은행의 계정을 표현한 클래스 
 class BankAccount:
+    #초기화
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #멤버숨김 : __변수명
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
+    #입금
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
+    #지급
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    #문자열의 결과를 리턴
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
+account1.deposit(5000)
 account1.withdraw(3000)
 print(account1)
+#print(account1.__balance)
+#백도어(약간 테스트 용도로만)
+print(account1._BankAccount__balance)
--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,33 @@
+#부모 클래스
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+    def working(self):
+        print("현재 작업중")
+    def coding(self):
+        print("현재 코딩중")
 
+#자식 클래스
 class Student(Person):
+    #덮어쓰기(상속받은 메서드 재정의, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모 초기화 메서드    
+        Person.__init__(self, name, phoneNumber)        
         self.subject = subject
         self.studentID = studentID
-
+    #덮어쓰기(재정의)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+s = Student("이순신", "010-111-1234", "빅데이터", "201122")
+p.printInfo()
+s.printInfo()
+s.coding()
+s.working()
 
 
